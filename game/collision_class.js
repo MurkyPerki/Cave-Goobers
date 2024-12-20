66e43924e7af14c2c29f75e339f2de1d8a054026
Tuna Sevinc <tuna.sevinc@hva.nl>
2024-12-20 08:58:15 +0100

    Lil change

--- game/collision_class.js
+++ game/collision_class.js
@@ -21,7 +21,7 @@ class Collision {
     
             Collision.verticalCollision(entity, platforms, nextY, vy);
             Collision.horizontalCollision(entity, platforms, nextX, vx);
-            //Collision.groundCollision(entity);
+            // Collision.groundCollision(entity);
             // Collision.wallCollision(entity);
         }
     
