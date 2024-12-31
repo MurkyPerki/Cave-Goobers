018ca37af8ae82fc5f828fc9fd726efed1d9346a
Tuna Sevinc <tuna.sevinc@hva.nl>
2024-12-31 16:36:27 +0100

    Clean up entityManager()

--- game/entityManager.js
+++ game/entityManager.js
@@ -4,13 +4,11 @@ class EntityManager {
         this.player = new Player(400, 700, 100, 100);
         this.enemies = [];
         this.items = [];
-        //this.player = new Player(400, 700, 100, 100);
     }
 
     initializeEntities() {
-        //this.entities.push(new Player(400, 700, 100, 100));
         this.enemies.push(new Enemy(10, 200, 30, 30));
-       // this.entities.push(new WindEnemy(200, 700, 30, 30));
+        this.enemies.push(new WindEnemy(200, 700, 30, 30));
         this.items.push(new Item(750, 500, 100, 80, this.items))
         this.items.push(new Item(100, 350, 100, 80, this.items))
         this.items.push(new Item(900, 200, 100, 80, this.items))
@@ -35,24 +33,4 @@ class EntityManager {
             item.render();
         }
     }
-
-    // render() {
-    //     this.render();
-    //     this.renderDebug();
-    //     this.renderCameraBox();
-    // }
-
-
-
-
-    // let entities = [
-    //     new Player(playerX, playerY, playerW, playerH),
-    //     new Enemy(),
-    //     new Enemy(),
-    // ]
-    // for (let index = 0; index < entities.length; index++) {
-    //     const entity = entities[index];
-    //     entity.update();
-    // }
-    // console.log('player.y:' + playerY);
 }
--- game/platforms.js
+++ game/platforms.js
@@ -12,24 +12,4 @@ class Platform {
         rect(this.x, this.y, this.width, this.height);
     }
 
-    // createPlatformsTilemap2D(tilemap2D) {
-    //     let tileWidth = width / tilemap2D[0].length;
-    //     let tileHeight = tileWidth;
-
-    //     platforms = [];
-    //     for (let row = 0; row < tilemap2D.length; row++) {
-    //         for (let col = 0; col < tilemap2D[row].length; col++) {
-    //             let tileValue = tilemap2D[row][col];
-    //             if (tileValue === 2) {
-    //                 let x = col * tileWidth;
-    //                 let y = row * tileHeight;
-    //                 platforms.push(new Platform(x, y, tileWidth, tileHeight))
-
-
-    //             }
-
-    //         }
-
-    //     }
-    // }
 }
