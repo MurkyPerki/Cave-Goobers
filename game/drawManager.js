c03c04164e8ee4c7bc31dba315a7c3b395441798
Tuna Sevinc <tuna.sevinc@hva.nl>
2025-01-01 16:20:56 +0100

    Made drawmanager

---
+++ game/drawManager.js
@@ -0,0 +1,20 @@
+
+class DrawManager {
+
+    managesAll(){
+        background(255);
+        //camera
+        let cameraY = windowHeight / 2 - entityManager.player.cameraYPos;
+    
+        push();
+        translate(0, cameraY);
+        entityManager.player.handleCollsions(entityManager.platforms);
+        entityManager.update();
+        entityManager.render();
+        // platforms are defined here in sketch but i want to render from entityManager >:(
+            for (let platform of entityManager.platforms) {
+                platform.render();
+            }
+        pop();
+    }
+}
--- game/sketch.js
+++ game/sketch.js
@@ -1,5 +1,6 @@
 
 //managers
+let drawManager;
 let databaseManager;
 let entityManager;
 let sceneManager; // if necessarry
@@ -19,25 +20,14 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 
     //initialize managers
+    drawManager = new DrawManager();
     entityManager = new EntityManager();
+
     entityManager.initializeEntities();
 }
 
 function draw() {
-    background(255);
-    //camera
-    let cameraY = windowHeight / 2 - entityManager.player.cameraYPos;
-
-    push();
-    translate(0, cameraY);
-    entityManager.player.handleCollsions(entityManager.platforms);
-    entityManager.update();
-    entityManager.render();
-    // platforms are defined here in sketch but i want to render from entityManager >:(
-        for (let platform of entityManager.platforms) {
-            platform.render();
-        }
-    pop();
+    drawManager.managesAll();
 }
 
 //this has to stay here
