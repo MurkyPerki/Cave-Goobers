f4584e257b003a6d12c4e67c88b8885a69dae622
Tuna Sevinc <tuna.sevinc@hva.nl>
2025-01-07 17:36:25 +0100

    clean draw

--- game/drawManager.js
+++ game/drawManager.js
@@ -1,24 +1,20 @@
 
 class DrawManager {
 
-    managesAll(){
+    managesAll() {
         background(255);
-       
+
         //camera
         let cameraY = windowHeight / 2 - entityManager.player.cameraYPos;
-    
+
         push();
         translate(0, cameraY);
         entityManager.update();
         entityManager.render();
-        // platforms are defined here in sketch but i want to render from entityManager >:(
-            // for (let platform of entityManager.platforms) {
-            //     platform.render();
-            // }
         pop();
-        
-        if(!start){
+
+        if (!start) {
             gameScreen.startscreen();
-             }
+        }
     }
 }
