33e0a483bc5e6d122048011d5766409c7700ea73
Max Burkhardt <mekkertor@gmail.com>
2025-01-08 15:00:41 +0100

    Reapply "Merge branch 'main' of gitlab.fdmci.hva.nl:propedeuse-hbo-ict/onderwijs/student-projecten/2024-2025/out-d-gd-se/blok-2/niipiimaayaa24"
    This reverts commit e6bfb111464449c3099cac16262e3567a9e3ea47.

---
+++ game/sprite.js
@@ -0,0 +1,14 @@
+
+class Sprite{
+    constructor({position, imageSrc}) {
+     this.position = position;
+     this.image = loadImage(imageSrc);
+     
+    }
+
+    render(){
+        this.image.width = windowWidth;
+        image(this.image, this.position.x, this.position.y);
+    }
+    
+}
