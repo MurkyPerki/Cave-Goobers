51dad85f00be598f13056e3239e7b0ae4bdb4566
Max Burkhardt <mekkertor@gmail.com>
2025-01-10 01:29:27 +0100

    added class gooberslots

--- game/gooberSlots.js
+++ game/gooberSlots.js
@@ -1,8 +1,30 @@
 class GooberSlots {
 
     constructor() {
-        
+        this.x = x;
+        this.y = y;
+        this.width = 50;
+        this.height = 50;
+        this.isFilled = false;
+    }
+
+    update(player, boss) {
+
+        if (Collision.isColliding(this.x, this.y, this.width, this.height, player)) {
+            if (player.gooberCount > 0 && !this.isFIlled) {
+
+                player.gooberCount--;
+                this.isFilled = true;
+            }
+        }
 
 
+        render() {
+
+            push();
+            fill(this.isFilled ? 'yellow : white');
+            Rect(this.x, this.y, this.width, this.height);
+            pop();
+        }
     }
 }
